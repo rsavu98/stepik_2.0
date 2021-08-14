99bddb46e5ad9aa6dff088f27ecb93a290b5006b
rsavu98 <savuliak9852@gmail.com>
2021-08-14 18:32:25 +0300

    Added task 4.2

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default='en',
                      help="Select any language")
 
 
